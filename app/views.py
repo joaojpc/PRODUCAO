b0ef554142d74e1ee7d239bdebae735a97b74b05
joaojpc <joaojpc1@gmail.com>
2026-03-17 03:28:55 -0300

    demandas

--- app/views.py
+++ app/views.py
@@ -28,6 +28,8 @@ class ApontaDemandaListView(APIView):
         #ordem, filial e lote;
         elif v_ordem is not None and v_filial is not None and v_lote is not None:
             serializer = self.serializer_class(Apt_Pro_Demandas.objects.filter(ORD_IN_CODIGO = v_ordem,FIL_IN_CODIGO = v_filial, PRO_ST_LOTE = v_lote), many=True)
+        elif v_ordem is not None and v_filial is not None:
+            serializer = self.serializer_class(Apt_Pro_Demandas.objects.filter(ORD_IN_CODIGO = v_ordem,FIL_IN_CODIGO = v_filial), many=True)
         elif v_status is not None:
             serializer = self.serializer_class(Apt_Pro_Demandas.objects.filter(ORD_IN_CODIGO = v_ordem, FIL_IN_CODIGO = v_filial, CTL_IN_CODIGO = v_transacao, MOV_ST_STATUS = v_status).order_by('MOV_IN_SEQUENCIA'), many=True)
         elif v_ordem == None:
