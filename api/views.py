7910b0d8b02d23f3ca15d05cfde4fa1bc5f448e4
joaojpc <joaojpc1@gmail.com>
2026-03-16 23:35:34 -0300

    print dados

--- api/views.py
+++ api/views.py
@@ -377,6 +377,7 @@ class GetSaldoLote(APIView):
     def get(self, request, format=None):
         ini_get = GetDadosProducao()
         dados = request.GET
+        print(dados)
         c_get = ini_get.get_saldo(dados)
         response = Response(c_get, status=status.HTTP_200_OK)
         return response
