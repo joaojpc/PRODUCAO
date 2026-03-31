6086fa6218d19b5188b2d57f918c8faeb9f63269
joaojpc <joaojpc1@gmail.com>
2026-03-31 18:47:20 -0300

    oracle

--- almoxarifado/diretorio.py
+++ almoxarifado/diretorio.py
@@ -1,7 +1,8 @@
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-from oracle_connection import OracleConnection
+from oracle_connection import getOracleConnection
+from url_projeto import geturlapp, geturlapi, geturlprod, geturlest, geturlinv
 import os
 print(os.path.abspath(__file__))
   # agora você pode importar arquivo_de_prod.py de prod/
