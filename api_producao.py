5a796c3001fc6e38f0a6b3585ff31d2e9dec9229
joaojpc <joaojpc1@gmail.com>
2026-06-30 15:42:48 -0300

    comenta impressão de apontamentos na função integraOrdens da classe integrador

--- api_producao.py
+++ api_producao.py
@@ -329,7 +329,7 @@ class integrador:
             print('Linha 330', v_rs)
             if v_rs:                
                 for rs in v_rs:
-                    print("Apontamentos", rs['ORD_IN_CODIGO'])
+                    #print("Apontamentos", rs['ORD_IN_CODIGO'])
                     self.ord_in = rs['ORD_IN_CODIGO']
                     self.payload = {'ordem': rs['ORD_IN_CODIGO'],'filial': rs['FIL_IN_CODIGO'],'ctl_in_codigo': rs['CTL_IN_CODIGO'],'status': 'A'}                
                     #Busca a situação da ordem
