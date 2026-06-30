b3348ae72e8389b46c5b48e08bd20e0310c27fb1
joaojpc <joaojpc1@gmail.com>
2026-06-30 15:21:17 -0300

    altera URLs de http para https nos arquivos js_demanda.html, js_integra.html, js_prod.html e url_projeto.py

--- url_projeto.py
+++ url_projeto.py
@@ -10,25 +10,25 @@ from producao import settings
 import requests
 def geturlapp(funcao):
     url_remoto = settings.URL_SQLITE
-    url_principal = 'http://'+url_remoto+'/app/'+funcao
+    url_principal = 'https//'+url_remoto+'/app/'+funcao
     return url_principal
 def geturlapi(funcao):
     url_remoto = settings.URL_SQLITE
-    url_principal = 'http://'+url_remoto+'/api/'+funcao
+    url_principal = 'https://'+url_remoto+'/api/'+funcao
     return url_principal
 def geturlprod(funcao):
     url_remoto = settings.URL_SQLITE
-    url_principal = 'http://'+url_remoto+'/prod/'+funcao
+    url_principal = 'https://'+url_remoto+'/prod/'+funcao
     return url_principal
 def geturlest(funcao):
     url_remoto = settings.URL_SQLITE
-    url_principal = 'http://'+url_remoto+'/est/'+funcao
+    url_principal = 'https://'+url_remoto+'/est/'+funcao
     return url_principal
 def geturlinv(funcao):
     url_remoto = settings.URL_SQLITE
-    url_principal = 'http://'+url_remoto+'/inv/'+funcao
+    url_principal = 'https://'+url_remoto+'/inv/'+funcao
     return url_principal
 def geturlrcb(funcao):
     url_remoto = settings.URL_SQLITE
-    url_principal = 'http://'+url_remoto+'/rcb/'+funcao
+    url_principal = 'https://'+url_remoto+'/rcb/'+funcao
     return url_principal
