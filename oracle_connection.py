f0732d58b1fba88ec4259494876295ba9ec972e0
joaojpc <joaojpc1@gmail.com>
2026-07-01 11:08:35 -0300

    refatora a inicialização do cliente Oracle e remove código desnecessário

--- oracle_connection.py
+++ oracle_connection.py
@@ -1,26 +1,21 @@
 import oracledb as cxo
 import platform
-import sys
-import os
+_oracle_inited = False
 
-nome_so = platform.system()
-if nome_so == "Linux":
-    # For Linux, specify the path to the Oracle Instant Client libraries
-    cxo.init_oracle_client(lib_dir="/opt/oracle/instantclient_21_7")
-elif nome_so == "Windows":
-    cxo.init_oracle_client(lib_dir=r"C:\Client_Oracle\instantclient_19_27")
-else:
-    print("Unsupported operating system. Please use Linux or Windows.")
-    sys.exit(1)
+def _init_oracle():
+    global _oracle_inited
+    if _oracle_inited: 
+        return
+    if platform.system() == "Linux":
+        cxo.init_oracle_client(lib_dir="/opt/oracle/instantclient_21_7")
+    elif platform.system() == "Windows":
+        cxo.init_oracle_client(lib_dir=r"C:\Client_Oracle\instantclient_19_27")
+    _oracle_inited = True
         
 def getOracleConnection():
-    username = 'idp'
-    password = 'megamega'
-    dsn = '10.101.235.105:1521/ORCL_gru1x6.subnetskydbindu.vcnrootautoskyo.oraclevcn.com'
-    try:
-        connection = cxo.connect(user=username, password=password, dsn=dsn)
-        return connection
-    
-    except cxo.DatabaseError as e:
-        print(f"Error connecting to Oracle Database: {e}")
-        return None
+    _init_oracle() # só inicia aqui
+    return cxo.connect(
+        user='idp', 
+        password='megamega', 
+        dsn='10.101.235.105:1521/ORCL_gru1x6.subnetskydbindu.vcnrootautoskyo.oraclevcn.com'
+    )
